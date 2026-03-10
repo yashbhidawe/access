a2d6c5c955ad23556294f65396585cf2b10b8d89
Yash! <ybh5179@gmail.com>
2026-03-10 21:42:12 +0530

    feat: sign-in

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,12 +1,18 @@
 import { Body, Controller, Post } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { CreateUserDto } from 'src/users/dto/create-user.dto';
+import { SignInDto } from './dto/signin.dto';
 
 @Controller('auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
+  //TODO: fix signup pipline
   @Post('signup')
   signup(@Body() createUserDto: CreateUserDto) {
     return this.authService.signUp(createUserDto);
+  }
+  @Post('signin')
+  signin(@Body() signInDto: SignInDto) {
+    return this.authService.signIn(signInDto);
   }
 }
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,6 +1,8 @@
 import { Injectable } from '@nestjs/common';
 import { CreateUserDto } from 'src/users/dto/create-user.dto';
 import { UsersService } from 'src/users/users.service';
+import { SignInDto } from './dto/signin.dto';
+import * as bcrypt from 'bcrypt';
 
 @Injectable()
 export class AuthService {
@@ -8,5 +10,19 @@ export class AuthService {
 
   async signUp(createUserDto: CreateUserDto) {
     return this.userService.create(createUserDto);
+  }
+
+  async signIn(signInDto: SignInDto) {
+    const user = await this.userService
+      .findAll()
+      .then((users) => users.find((user) => user.email === signInDto.email));
+    if (!user) {
+      return { message: 'User not found' };
+    }
+    const isMatch = await bcrypt.compare(signInDto.password, user.password);
+    if (!isMatch) {
+      return { message: 'Invalid credentials' };
+    }
+    return { message: 'Sign in successful' };
   }
 }
